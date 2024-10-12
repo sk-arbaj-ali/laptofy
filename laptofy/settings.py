7f3ce4d4b94de0dacdcaff9c3f3fdd4c40ceb105
SK ARBAJ ALI <skarbajali.one@outlook.com>
2024-10-12 22:11:30 +0530

    Fix: Changed ALLOWED_HOSTS in settings file.

--- laptofy/settings.py
+++ laptofy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['render.com']
+ALLOWED_HOSTS = ['laptofy.onrender.com']
 
 
 # Application definition
