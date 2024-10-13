80c8442da0a300eb777e046ab22687a7287cbdfe
SK ARBAJ ALI <skarbajali.one@outlook.com>
2024-10-13 08:12:11 +0530

    Fix: Changed database from sqlite3 to MySQL.

---
+++ build.sh
@@ -0,0 +1,2 @@
+python manage.py migrate
+gunicorn laptofy.wsgi:application
--- laptofy/settings.py
+++ laptofy/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'laptofy.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / 'db.sqlite3',
-    },
     # 'default': {
-    #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": "ecommerce",
-    #     "USER": "root",
-    #     "PASSWORD": "arbaj",
-    #     "HOST": "127.0.0.1",
-    #     "PORT": "3306",
-    # }
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / 'db.sqlite3',
+    # },
+    'default': {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        # "PORT": "3306",
+    }
 }
 
 
